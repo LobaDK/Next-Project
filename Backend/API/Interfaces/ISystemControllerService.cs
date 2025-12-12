b9bb1852e8de166db792512e734e399362b85690
Nicklas <gta5.r6s.bf4@gmail.com>
2025-12-12 11:06:16 +0100

    Refactor SystemController and ISystemControllerService to remove async from methods that do not require it

--- Backend/API/Interfaces/ISystemControllerService.cs
+++ Backend/API/Interfaces/ISystemControllerService.cs
@@ -2,13 +2,13 @@ namespace API.Interfaces;
 
 public interface ISystemControllerService
 {
-    Task<bool> StopServer();
-    Task<FileResult> ExportSettings();
+    bool StopServer();
+    FileResult ExportSettings();
     Task<bool> ImportSettings(IFormFile file);
     Task<FileResult> GetLogFile(string filename);
     List<string> GetLogFileNames();
-    Task<SettingsFetchResponse> GetSettings();
-    Task<SettingsSchema> GetSettingsSchema();
+    SettingsFetchResponse GetSettings();
+    SettingsSchema GetSettingsSchema();
     Task<bool> UpdateSettings(UpdateSettingsRequest rootSettings);
     Task<bool> PatchSettings(PatchSettingsRequest rootSettings);
 }
