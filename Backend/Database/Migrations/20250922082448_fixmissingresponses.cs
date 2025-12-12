0b84049d12ed4a00dedca2a470ca6d990b3dd441
Nicklas <gta5.r6s.bf4@gmail.com>
2025-12-12 11:07:34 +0100

    Add pragma warning directives to suppress CS8981 for fixmissingresponses migration class

--- Backend/Database/Migrations/20250922082448_fixmissingresponses.cs
+++ Backend/Database/Migrations/20250922082448_fixmissingresponses.cs
@@ -8,7 +8,9 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Database.Migrations
 {
     /// <inheritdoc />
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     public partial class fixmissingresponses : Migration
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
