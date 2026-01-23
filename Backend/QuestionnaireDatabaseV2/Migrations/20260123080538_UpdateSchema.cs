7088b076501b1a22dad077d9c2bf155b98b07458
Nicklas <gta5.r6s.bf4@gmail.com>
2026-01-23 09:06:28 +0100

    Remove AllowAnonymizedManagerViewing column and add Type column to Assignments table

---
+++ Backend/QuestionnaireDatabaseV2/Migrations/20260123080538_UpdateSchema.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestionnaireDatabaseV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowAnonymizedManagerViewing",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Assignments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowAnonymizedManagerViewing",
+                table: "Assignments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
