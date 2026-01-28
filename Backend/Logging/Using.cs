cb9ff25d6091411948adb0c0abed009c36494cb8
Nicklas <gta5.r6s.bf4@gmail.com>
2026-01-28 14:01:22 +0100

    Remove unused FileLogger import from Using.cs

--- Backend/Logging/Using.cs
+++ Backend/Logging/Using.cs
@@ -8,9 +8,7 @@ global using Database.Interfaces;
 global using Logging.DBLogger;
 
 global using Microsoft.Extensions.DependencyInjection;
-global using Logging.FileLogger;
 
-global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Logging.Configuration;
