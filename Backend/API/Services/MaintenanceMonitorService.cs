6124ba95ff929daed03604a1039cb67be5f25f23
Nicklas <gta5.r6s.bf4@gmail.com>
2026-03-19 11:03:39 +0100

    Add MaintenanceMonitor service to manage application maintenance mode

---
+++ Backend/API/Services/MaintenanceMonitorService.cs
@@ -0,0 +1,101 @@
+namespace API.Services;
+
+public class MaintenanceMonitor(ILogger<MaintenanceMonitor> logger) : IMaintenanceMonitor
+{
+    private readonly ILogger<MaintenanceMonitor> _logger = logger;
+    private bool _isMaintenanceEnabled = true; // CHANGE THIS TO FALSE BEFORE COMMITING YOU BELLEND
+    private string _reason = string.Empty;
+    private readonly object _lock = new();
+
+    public bool IsMaintenanceEnabled
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _isMaintenanceEnabled;
+            }
+        }
+    }
+
+    public string Reason
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _reason;
+            }
+        }
+    }
+
+    public void EnableMaintenance()
+    {
+        lock (_lock)
+        {
+            if (!_isMaintenanceEnabled)
+            {
+                _logger.LogDebug("Enabling maintenance mode");
+                _isMaintenanceEnabled = true;
+            }
+        }
+    }
+
+    public void EnableMaintenance(string reason)
+    {
+        lock (_lock)
+        {
+            if (!_isMaintenanceEnabled)
+            {
+                _logger.LogDebug("Enabling maintenance mode with reason: {Reason}", reason);
+                _isMaintenanceEnabled = true;
+            }
+            _reason = reason;
+        }
+    }
+
+    public void DisableMaintenance(bool clearReason = false)
+    {
+        lock (_lock)
+        {
+            if (_isMaintenanceEnabled)
+            {
+                _logger.LogDebug("Disabling maintenance mode");
+                _isMaintenanceEnabled = false;
+            }
+            if (clearReason && !string.IsNullOrEmpty(_reason))
+            {
+                _logger.LogDebug("Clearing maintenance reason");
+                _reason = string.Empty;
+            }
+        }
+    }
+
+    public void SetMaintenanceReason(string reason)
+    {
+        lock (_lock)
+        {
+            if (string.IsNullOrEmpty(_reason))
+            {
+                _logger.LogDebug("Setting maintenance reason to {Reason}", reason);
+            }
+            else
+            {
+                _logger.LogDebug("Updating maintenance reason from {OldReason} to {NewReason}", _reason, reason);
+            }
+            _reason = reason;
+        }
+    }
+
+    public void ClearMaintenanceReason()
+    {
+        lock (_lock)
+        {
+            if (!string.IsNullOrEmpty(_reason))
+            {
+                _logger.LogDebug("Clearing maintenance reason");
+                _reason = string.Empty;
+            }
+        }
+    }
+}
